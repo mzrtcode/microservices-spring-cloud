dd30acce8700abf0b0a01551dc6aad1f9569f594
Stiven Medina <stiven.medina@pragma.com.co>
2024-07-19 19:38:33 -0500

    Crear proyecto y crear archivo docker-compose.yml

---
+++ ms-companies/src/main/java/com/mzrtcode/ms_companies/MsCompaniesApplication.java
@@ -0,0 +1,13 @@
+package com.mzrtcode.ms_companies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompaniesApplication.class, args);
+	}
+
+}
