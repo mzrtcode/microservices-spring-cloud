1c309f12b4a63bf7c2ce6f3490d1634254c0f8ae
Stiven Medina <stiven.medina@pragma.com.co>
2024-07-20 09:47:41 -0500

    Implementar capa de servicio para Company

--- ms-companies/src/main/java/com/mzrtcode/ms_companies/service/CompanyServiceImpl.java
+++ ms-companies/src/main/java/com/mzrtcode/ms_companies/service/CompanyServiceImpl.java
@@ -1,25 +1,58 @@
 package com.mzrtcode.ms_companies.service;
 
+import com.mzrtcode.ms_companies.model.Category;
 import com.mzrtcode.ms_companies.model.Company;
+import com.mzrtcode.ms_companies.repository.ICompanyRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+@Service
+@Transactional
+@Slf4j
+@AllArgsConstructor
 public class CompanyServiceImpl implements ICompanyService{
+
+    private final ICompanyRepository companyRepository;
+
     @Override
     public Company readByName(String name) {
-        return null;
+        return this.companyRepository.findByName(name)
+                .orElseThrow(() -> new NoSuchElementException("Company not found"));
     }
 
     @Override
     public Company create(Company company) {
-        return null;
+        company.getWebsites().forEach(website ->{
+            if (Objects.isNull(website.getCategory())){
+                website.setCategory(Category.NONE);
+            }
+        });
+        return this.companyRepository.save(company);
     }
 
     @Override
     public Company update(Company company, String name) {
-        return null;
+        var companyToUpdate = this.companyRepository.findByName(name)
+                .orElseThrow(() -> new NoSuchElementException("Company not found"));
+
+        companyToUpdate.setLogo(company.getLogo());
+        companyToUpdate.setFoundationDate(company.getFoundationDate());
+        companyToUpdate.setFounder(company.getFounder());
+        return this.companyRepository.save(companyToUpdate);
     }
 
     @Override
     public void delete(String name) {
+        var companyToDelete =  this.companyRepository.findByName(name)
+                .orElseThrow(() -> new NoSuchElementException("Company not found"));
+
+        this.companyRepository.delete(companyToDelete);
 
     }
 }
