3f5482029f9e820dcad887860031b7832ac6a1f4
Stiven Medina <stiven.medina@pragma.com.co>
2024-07-20 09:25:39 -0500

    Definir capa de Servicio para la entidad Company

---
+++ ms-companies/src/main/java/com/mzrtcode/ms_companies/service/CompanyServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mzrtcode.ms_companies.service;
+
+import com.mzrtcode.ms_companies.model.Company;
+
+public class CompanyServiceImpl implements ICompanyService{
+    @Override
+    public Company readByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Company create(Company company) {
+        return null;
+    }
+
+    @Override
+    public Company update(Company company, String name) {
+        return null;
+    }
+
+    @Override
+    public void delete(String name) {
+
+    }
+}
